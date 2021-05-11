81ce5a35616db9141171030d332e0558d3509099
Florian Arndt <post@florianarndt.com>
2021-05-11 16:04:26 +0200

    php artisan dte:install

--- src/Commands/DTEInstallEditorFromZIPFileCommand.php
+++ src/Commands/DTEInstallEditorFromZIPFileCommand.php
@@ -4,6 +4,10 @@ namespace arweb\DataTablesEditor\Commands;
 
 use Illuminate\Console\Command;
 
+/**
+ * Class DTEInstallEditorFromZIPFileCommand
+ * @package arweb\DataTablesEditor\Commands
+ */
 class DTEInstallEditorFromZIPFileCommand extends Command
 {
     /**
@@ -27,10 +31,50 @@ class DTEInstallEditorFromZIPFileCommand extends Command
      */
     public function handle()
     {
-        $this->info('Copying "' . $this->argument('zip_file_or_folder') . '" into this project');
+        $zipFilePath = $this->argument('zip_file_or_folder');
+        $targetParentPath = public_path();
+        $targetPath = public_path('DTE2');
+
+        $this->info('Copying "' . $zipFilePath . '" into this project');
+
         // 1. does source exist?
-        // 2. is source file or directory?
-        // 3a. file? try unzipping to /public
-        // 4a. directory? just copy to /public
+        if (!file_exists($zipFilePath)) {
+            $this->error('Could not find ZIP archive "' . $zipFilePath . '"');
+        }
+        if (is_dir($zipFilePath)) {
+            $this->error('Your source "' . $zipFilePath . '" is a directory, not a file.');
+        }
+
+        // 2. does target not exist?
+        if (file_exists($targetPath)) {
+            $this->error('The target "' . $targetPath . '" already exists - doing nothing.');
+            exit(1);
+        }
+
+        // 3. handle source file
+        // a) open
+        $zip = new \ZipArchive();
+        $handle = $zip->open($zipFilePath);
+        if ($handle === false) {
+            $this->error('Could not open "' . $zipFilePath . '".');
+        }
+
+        // b) extract
+        // TODO: determine $editorPathInsideZip based on ZIP file contents
+        $editorPathInsideZip = 'Editor-PHP-2.0.2';
+        $zip->extractTo($targetParentPath);
+
+        // c) rename extracted directory to $targetPath
+        rename($targetParentPath . '/' . $editorPathInsideZip, $targetPath);
+
+        // d) close
+        $zip->close();
+
+        $this->info('The archive was extracted to ' . $targetParentPath);
+    }
+
+    public function error($message, $verbosity = null)
+    {
+        parent::error('[!] Error: ' . $message, $verbosity);
     }
 }
